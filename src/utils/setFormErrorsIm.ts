17eb32b835c061b2c2faae5d3db9449d188c0147
Patrick Baldwin <patrickbaldwin614@gmail.com>
2020-04-17 21:50:30 -0400

    made setFormErrorsIm.ts a generic

--- src/utils/setFormErrorsIm.ts
+++ src/utils/setFormErrorsIm.ts
@@ -3,20 +3,16 @@ import type { ValidationError } from 'yup';
 // eslint-disable-next-line no-unused-vars
 import type { Record } from 'immutable';
 
-interface Accumulator {
-  [key: string]: any,
-}
-
-interface SetFormErrorsImArgs {
-  setter: React.Dispatch<React.SetStateAction<Record<Accumulator>>> & Readonly<Accumulator>,
+interface SetFormErrorsImArgs<T> {
+  setter: React.Dispatch<React.SetStateAction<Record<T> & Readonly<T>>>,
   errors: [ValidationError],
 }
 
-export const setFormErrorsIm = ({ setter, errors }: SetFormErrorsImArgs) => {
+export const setFormErrorsIm = <T>({ setter, errors }: SetFormErrorsImArgs<T>) => {
   setter((oldState) => (
     oldState
       .clear()
-      .merge(errors.reduce((acc: Accumulator, error) => {
+      .merge(errors.reduce((acc: any, error) => {
         acc[error.path] = acc[error.path] === undefined ? error.errors[0] : acc[error.path];
         return acc;
       }, {}))
