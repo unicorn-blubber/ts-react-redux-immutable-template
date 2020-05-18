625b403ded34d2a78bba565d9be7a27283c9bff7
Patrick Baldwin <patrickbaldwin614@gmail.com>
2020-05-18 03:20:17 -0400

    renaming for clarity

--- src/utils/newRecord.ts
+++ src/utils/newRecord.ts
@@ -3,6 +3,6 @@ import { Record } from 'immutable';
 // eslint-disable-next-line no-unused-vars
 import { ReadonlyRecord } from '../react-app-env';
 
-export const newRecord = <T>(obj: T) => Record(obj)() as ReadonlyRecord<T>;
+export const newRecord = <T>(defaults: T) => Record(defaults)() as ReadonlyRecord<T>;
 
 export default {};
