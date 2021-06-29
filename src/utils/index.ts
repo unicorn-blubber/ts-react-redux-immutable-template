ed8f7f628917d0f64a8586b3fec760fbfdf13182
Patrick Baldwin <patrickbaldwin614@gmail.com>
2021-06-29 05:55:14 -0400

    linting and adding safeImmutables to exports

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,25 @@ export { axiosWithAuth } from './axiosWithAuth';
 export { axiosWithOAuth } from './axiosWithOAuth';
 export { axiosOAuthLogin } from './axiosOAuthLogin';
 export { buildMapStateToProps } from './buildMapStateToProps';
+// eslint-disable-next-line import/named
+export { buildThunk } from './buildThunk';
 export { filterObject } from './filterObject';
 export { newRecord } from './newRecord';
 export { setFormErrors } from './setFormErrors';
+// eslint-disable-next-line import/named
 export { setFormErrorsIm } from './setFormErrorsIm';
+export {
+  SafeList,
+  SafeMap,
+  SafeRecord,
+  SafeSet,
+  SafeStack,
+} from './safeImmutables';
+export type {
+  ImmutableType,
+  SafeListType,
+  SafeMapType,
+  SafeObject,
+  SafeSetType,
+  SafeStackType,
+} from './safeImmutables';
